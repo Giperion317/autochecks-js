82cbf45865a72186fc566bd26eaf4064b11d046e
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-08-14 02:25:47 +0200

    task19

---
+++ js/autochecks.js
@@ -0,0 +1,281 @@
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// ownerName - имя владельца;
+// ownerPhone - телефон владельца;
+// ownerEmail - почта владельца;
+// numberOfTags - количество элементов массива в свойстве tags;
+// firstTag - первый элемент массива в свойстве tags;
+// lastTag - последний элемент массива в свойстве tags.
+
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+
+// const ownerName = apartment.owner.name;
+// console.log(ownerName)
+
+// const ownerPhone = apartment.owner.phone;
+// console.log(ownerPhone)
+
+// const ownerEmail = apartment.owner.email;
+// console.log(ownerEmail)
+
+// const numberOfTags = apartment.tags.length;
+// console.log(numberOfTags)
+
+// const firstTag = apartment.tags[0];
+// console.log(firstTag)
+
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// console.log(lastTag)
+
+// ============================Задание 6
+
+// Дополни код обновив значения свойств объекта apartment:
+
+// цену в свойстве price на 5000;
+// рейтинг квартиры в свойстве rating на 4.7;
+// имя владельца во вложенном свойстве name на "Henry Sibola";
+// массив тегов в свойстве tags добавив в конец строку "trusted".
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+// console.log(apartment)
+
+// ===============Задание 10
+
+// Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+
+// for (const key in apartment) {
+//     keys.push(key);
+//     values.push(apartment[key])
+// }
+
+// console.log(keys)
+// console.log(values)
+
+// =========Task 12
+
+// Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
+
+// Объявлена функция countProps(object)
+// Вызов countProps({}) возвращает 0
+// Вызов countProps({ name: "Mango", age: 2 }) возвращает 2
+// Вызов countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) возвращает 3
+// Функция подсчитывает только собственные свойства объекта
+
+// function countProps(object) {
+//     let propCount = 0;
+   
+//     let keys = [];
+//     for (const key in object) {
+        
+//     if (object.hasOwnProperty(key)){
+//         keys.push(key);
+//             }
+
+        
+//     }
+    
+//     return propCount = keys.length ;
+// }
+  
+  
+
+// console.log(countProps({name: "Mango", age: 2}))
+// console.log(countProps({mail: "poly@mail.com", isOnline: true, score: 500}))
+// console.log(countProps({}))
+
+// =========Task13
+
+// Перебери объект apartment используя метод Object.keys() и цикл for...of. Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
+
+// Объявлена переменная apartment.
+// Значение переменной apartment это объект.
+// Объявлена переменная keys.
+// Значение переменной keys это массив ["descr", "rating", "price"].
+// Значение переменной keys получено с помощью метода Object.keys().
+// Объявлена переменная values.
+// Значение переменной values это массив ["Spacious apartment in the city center", 4, 2153].
+// Значение переменной values получено с помощью цикла for...of.
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log(keys)
+// for (const key of keys) {
+//     values.push(apartment[key]);
+// }
+
+// console.log(values)
+
+// =======Task14
+
+// Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, но необязательно, цикл for...of.
+
+// Объявлена функция countProps(object)
+// Вызов countProps({}) возвращает 0
+// Вызов countProps({ name: "Mango", age: 2 }) возвращает 2
+// Вызов countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) возвращает 3
+// Функция подсчитывает только собственные свойства объекта
+// Функция использует метод Object.keys() и, возможно, цикл for...of
+
+// function countProps(object) {
+//   // Change code below this line
+//     let propCount = 0;
+//     let keys = Object.keys(object);
+    
+//     propCount += keys.length;
+    
+
+//     return propCount;
+    
+    
+//   // Change code above this line
+// }
+// console.log(countProps({}))
+// console.log(countProps({ name: "Mango", age: 2 }))
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
+
+
+// =======Task17
+
+// Перебери массив объектов colors используя цикл for...of. Добавь в массив hexColors значения свойств hex, а в массив rgbColors значения свойств rgb из всех объектов массива colors.
+
+// Объявлена переменная colors
+// Значение переменной colors это массив
+// Объявлена переменная hexColors
+// Значение переменной hexColors это массив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// Объявлена переменная rgbColors
+// Значение переменной rgbColors это массив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (let color of colors) {
+//     hexColors.push(color.hex)
+//     rgbColors.push(color.rgb)
+// }
+
+// console.log(hexColors)
+// console.log(rgbColors)
+// // Change code below this line
+
+// =============Task18
+
+// Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). Если продукт с таким названием не найден, функция должна возвращать null.
+
+// Объявлена функция getProductPrice(productName).
+// Вызов getProductPrice("Radar") возвращает 1300.
+// Вызов getProductPrice("Grip") возвращает 1200.
+// Вызов getProductPrice("Scanner") возвращает 2700.
+// Вызов getProductPrice("Droid") возвращает 400.
+// Вызов getProductPrice("Engine") возвращает null.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//     for (const product of products) {
+    
+//     if (product.name === productName) {
+//         return product.price;
+//     }
+// }
+//     return null;
+
+//   // Change code above this line
+// }
+
+// console.log(getProductPrice("Radar"))
+// console.log(getProductPrice("Grip"))
+// console.log(getProductPrice("Scanner"))
+// console.log(getProductPrice("Droid"))
+// console.log(getProductPrice("Engine"))
+
+// =============Task19
+
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+
+// Объявлена функция getAllPropValues(propName)
+// Вызов getAllPropValues("name") возвращает ["Radar", "Scanner", "Droid", "Grip"]
+// Вызов getAllPropValues("quantity") возвращает [4, 3, 7, 9]
+// Вызов getAllPropValues("price") возвращает [1300, 2700, 400, 1200]
+// Вызов getAllPropValues("category") возвращает []
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+    let valueProduct = [];
+     for (const product of products) {
+        console.log(product)
+        if (Object.keys(product) === propName) {
+            console.log(true)
+        }
+        
+}
+
+
+  // Change code above this line
+}
+getAllPropValues("name")
